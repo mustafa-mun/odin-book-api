97608f0fdd55ea2398fe387011ace82a9e08b7be
Mustafa <mustafamcm9@gmail.com>
2023-04-17 00:14:46 +0300

    profile model

---
+++ models/profile.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// PROFILE MODEL
+const ProfileSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, required: true },
+  profile_picture: {
+    type: String,
+    default:
+      "https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png",
+  },
+  about: { type: String, required: true },
+  posts:[{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+});
+
+module.exports = mongoose.model("Profile", ProfileSchema);
