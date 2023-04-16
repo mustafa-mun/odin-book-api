c5cc939d06a55df8d97cee5454bc223f3c567b56
Mustafa <mustafamcm9@gmail.com>
2023-04-17 00:18:52 +0300

    update default post img url

--- models/post.js
+++ models/post.js
@@ -4,17 +4,21 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, required: true },
   content: { type: String, required: true },
-  post_img: { type: String },
+  post_img: { type: String, default: "#" },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
-  comments: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Comment'
-  }],
-  likes: [{
-    type: Schema.Types.ObjectId,
-    ref: 'PostLike'
-  }]
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "PostLike",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Post", PostSchema);
