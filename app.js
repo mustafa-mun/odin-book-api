d9a657288c68f35d13cd6fbfb474331dd955b1ee
Mustafa <mustafamcm9@gmail.com>
2023-04-17 00:48:44 +0300

    configurations

--- app.js
+++ app.js
@@ -7,13 +7,8 @@ const logger = require("morgan");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -32,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -45,9 +39,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status).json({ err });
 });
 
 module.exports = app;
