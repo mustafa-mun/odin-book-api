35654acf1064397ceb50453e3b137c33c8fa2f6f
Mustafa <mustafamcm9@gmail.com>
2023-04-17 00:15:22 +0300

    type fix   This commit fixes to array of ObjectIds typo

--- models/user.js
+++ models/user.js
@@ -6,7 +6,12 @@ const UserSchema = new Schema({
   last_name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  friends: [Schema.Types.ObjectId],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
   is_admin: { type: Boolean, default: false },
 });
 
