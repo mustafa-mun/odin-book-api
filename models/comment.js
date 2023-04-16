cad2449e3c9b0f1c4d275e0879a8958f7e0e3aec
Mustafa <mustafamcm9@gmail.com>
2023-04-17 00:14:12 +0300

    comment model

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, required: true },
+  content: { type: String, required: true },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "CommentLike",
+    },
+  ],
+  created_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
