7786cfd0c266eabfdbdc685059c6fe77cce061a2
Mustafa <mustafamcm9@gmail.com>
2023-04-19 20:38:30 +0300

    posts route dummy functions

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,3 +3,22 @@ const UserProfile = require("../models/profile");
 const Post = require("../models/post");
 const PostLike = require("../models/post_like"); // This can go into it's own controller 'likeController'
 
+exports.get_timeline_posts = async (req, res, next) => {
+  res.json({ message: "NOT IMPLEMENTED: Get timeline posts" });
+};
+
+exports.get_post = async (req, res, next) => {
+  res.json({ message: "NOT IMPLEMENTED: Get user post" });
+};
+
+exports.create_post = async (req, res, next) => {
+  res.json({ message: "NOT IMPLEMENTED: Create post" });
+};
+
+exports.update_post = async (req, res, next) => {
+  res.json({ message: "NOT IMPLEMENTED: Update post" });
+};
+
+exports.delete_post = async (req, res, next) => {
+  res.json({ message: "NOT IMPLEMENTED: Delete post" });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const postsController = require("../controllers/postsController");
-const authController = require("../controllers/authController");
+const authController = require("../controllers/authController"); // To protect routes
+
+router.get("/", postsController.get_timeline_posts);
+router.get("/:postId", postsController.get_post);
+router.post("/", authController.authenticateToken, postsController.create_post);
+router.put(
+  "/:postId",
+  authController.authenticateToken,
+  postsController.update_post
+);
+router.delete(
+  "/postId",
+  authController.authenticateToken,
+  postsController.delete_post
+);
 
 module.exports = router;
