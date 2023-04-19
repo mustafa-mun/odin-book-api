7a9e5c07c0678fbfd59a84e736557642f06fa275
Mustafa <mustafamcm9@gmail.com>
2023-04-19 12:18:06 +0300

    add basic structure of posts router

---
+++ controllers/postsController.js
@@ -0,0 +1,5 @@
+const User = require("../models/user");
+const UserProfile = require("../models/profile");
+const Post = require("../models/post");
+const PostLike = require("../models/post_like"); // This can go into it's own controller 'likeController'
+
---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const postsController = require("../controllers/postsController");
+const authController = require("../controllers/authController");
+
+module.exports = router;
