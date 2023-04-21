cc533ae1a2956311f0ac5e827a91e11109f37b45
Mustafa <mustafamcm9@gmail.com>
2023-04-21 15:06:16 +0300

    add new field and ref   This commit adds new field and appropriate refs to referances.

--- models/comment-models/comment.js
+++ models/comment-models/comment.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // COMMENT MODEL
 const CommentSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   content: { type: String, required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   likes: [
     {
       type: Schema.Types.ObjectId,
