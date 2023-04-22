9a5f9981dee9470736a901661e64f39c3cb3ceb2
Mustafa <mustafamcm9@gmail.com>
2023-04-22 20:45:47 +0300

    add friend recommendations

--- controllers/timelineController.js
+++ controllers/timelineController.js
@@ -1,25 +1,11 @@
 const Post = require("../models/post-models/post");
 const User = require("../models/user-models/user");
 
-/**
- * WHAT IS GOING TO BE IMPLEMENTED :
- * /**
- * WHAT IS GOING TO BE IMPLEMENTED :
- *  - A query parameter url for sorting posts with date (/posts?sort=like&order=asc) (ascending and descending )
- *  - A query parameter url for sorting posts with like (/posts?sort=like&order=asc) (ascending and descending )
- *  - A query parameter url for limiting posts (/posts?limit=10)
- *  - Show users and his friends posts
- *      . Posts should display content, author, comments and likes
- *      . Sort comments from newest to oldest
- *  - Show random friend suggestions
- *      . If they are not friends already
- *      . There must be a some sort of link or redirection to send friend request
- */
-
 exports.get_timeline = async (req, res, next) => {
   try {
     // Find user
     const user = await User.findById(req.jwt_token.user.id).select("friends");
+    // User and friends ids
     const friendIds = [user._id, ...user.friends];
     // Find users and friends post
     const posts = await Post.find({ author: { $in: friendIds } })
@@ -28,8 +14,28 @@ exports.get_timeline = async (req, res, next) => {
         path: "author",
         select: "first_name last_name",
       });
-    return res.json({ posts });
+    // Find users that not friends with current user
+    const friendRecommendations = await User.find({
+      _id: { $nin: friendIds },
+    })
+      .select("first_name last_name") // Include only 'first name' and 'last name' fields
+      .limit(5); // Limit with 5
+
+    // Add friend request route for each of the recommendations
+    const mapFriendRequest = friendRecommendations.map((friend) => {
+      return {
+        ...friend.toObject(),
+        send_friend_request: `POST /users/friend-request/${friend._id}`,
+      };
+    });
+    // Return posts and friend recommendations
+    return res.json({
+      posts,
+      friend_recommendations: mapFriendRequest,
+    });
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({
+      error: error.message,
+    });
   }
 };
