3111a7e1d52f6addd7c4566adbc33b755e5c121f
Mustafa <mustafamcm9@gmail.com>
2023-04-22 12:38:00 +0300

    add references to like schemas

--- models/post-models/post_like.js
+++ models/post-models/post_like.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // POST LIKE MODEL
 const PostLikeSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, required: true },
-  post: { type: Schema.Types.ObjectId, required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   liked_at: { type: Date, default: Date.now },
 });
 
