b48765d0c6c4c975553731436f0e386acc991bef
Mustafa <mustafamcm9@gmail.com>
2023-04-22 14:10:12 +0300

    comment like model bug fix

--- models/comment-models/comment_like.js
+++ models/comment-models/comment_like.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 // COMMENT LIKE MODEL
 const CommentLikeSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  comment: { type: Schema.Types.ObjectId, ref: "Comment", required: true },
   liked_at: { type: Date, default: Date.now },
 });
 
