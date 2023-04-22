e3e4db167865c620cf788338693dad94664f3473
Mustafa <mustafamcm9@gmail.com>
2023-04-22 12:35:20 +0300

    handle like post request

--- controllers/post/likesController.js
+++ controllers/post/likesController.js
@@ -1,6 +1,7 @@
 const Post = require("../../models/post-models/post");
 const PostLike = require("../../models/post-models/post_like");
 const CommentLike = require("../../models/comment-models/comment_like");
+const User = require("../../models/user-models/user");
 
 /**
  * WHAT IS GOING TO BE IMPLEMENTED :
@@ -21,7 +22,46 @@ exports.get_comment_likes = async (req, res, next) => {
 };
 
 exports.like_post = async (req, res, next) => {
-  return res.json({ message: "NOT IMPLEMENTED: LIKE POST" });
+  try {
+    // Find post
+    const post = await Post.findById(req.params.postId);
+    // Check if user is already liked this post
+    const likedAlready = await PostLike.findOne({
+      user: req.jwt_token.user.id,
+      post: req.params.postId,
+    });
+
+    if (!post) {
+      // Post not found
+      return res.status(404).json({ error: "Post not found!" });
+    }
+
+    if (likedAlready) {
+      // User already liked this post
+      return res.status(400).json({ error: "You already liked this post!" });
+    }
+
+    // Create new post like
+    const like = new PostLike({
+      user: req.jwt_token.user.id,
+      post: req.params.postId,
+    });
+    // Save like on database
+    const savedLike = await like.save();
+    // Add like to posts likes array and save post on database
+    const populatedLike = await PostLike.findById(savedLike._id).populate({
+      path: "post", // Populate post field
+      select: "content like_count", // Include only 'content' and 'like count'
+    });
+    populatedLike.like_count += 1; // This is for return document
+    post.likes.push(savedLike);
+    post.like_count += 1; // This is for database
+    await post.save();
+    // Return saved and populated like
+    return res.status(200).json({ like: populatedLike });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 exports.like_comment = async (req, res, next) => {
